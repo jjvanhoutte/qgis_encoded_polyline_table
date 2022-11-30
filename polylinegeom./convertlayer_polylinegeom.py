4667b7920069c10ed8c466604618617dffc12912
jjvanhoutte <87080460+jjvanhoutte@users.noreply.github.com>
2022-11-30 14:21:19 +0100

    Update convertlayer_polylinegeom.py

--- polylinegeom./convertlayer_polylinegeom.py
+++ polylinegeom./convertlayer_polylinegeom.py
@@ -1,14 +1,19 @@
 # Run the "encoded polyline" plugin before using this script
 # Run the polylinegeom_dev.py also
+# 2022.11.30 - made length of target fiuelds dependent on source field
+# The result is that the name includes the length argument (issue)
+# 
 layer_s = QgsProject.instance().mapLayersByName('all_routes')[0]
+maxLenName=max(list(map(len,QgsVectorLayerUtils.getValues(layer_s,"outFileName",)[0])))+5
+maxLenId=max(list(map(len,QgsVectorLayerUtils.getValues(layer_s,"id",)[0])))+5
 layer_d = QgsVectorLayer(
-    "LineString?crs=epsg:4326&field=id:string(36)&field=name:string(80)",
+    "LineString?crs=epsg:4326&field=id:string(maxLenId)&field=name:string(maxLenName)",
     'newPolyLineGeom2',
     "memory",
 )
 pr = layer_d.dataProvider()
 for feature_s in layer_s.getFeatures():
-    print(feature_s["outFileName"])
+    print(feature_s["out#FileName"])
     feature = QgsFeature()
     feature.setAttributes([feature_s["id"],feature_s["outFileName"]])
     feature.setGeometry(polylinegeom(feature_s["routePolyline"]))
